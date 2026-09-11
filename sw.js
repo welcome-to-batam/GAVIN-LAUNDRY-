5e601a58cc5a0e7a30ea086eca249921aa448e69
welcome-to-batam <cowokperenung@gmail.com>
2026-09-11 19:36:27 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'gavin-laundry-v1';
+const ASSETS = [ 'index.html', 'manifest.json', 'public/logo.png', 'public/laundry-bg.jpg' ];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
+
