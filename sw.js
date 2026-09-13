c96095844e256428f3e17e97f3e1f05548fc25eb
welcome-to-batam <cowokperenung@gmail.com>
2026-09-13 17:38:27 +0700

    Update cache name to 'gavin-laundry-v33'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gavin-laundry-v32';
+const CACHE_NAME = 'gavin-laundry-v33';
 const ASSETS = [ 'index.html', 'manifest.json', 'public/logo.png', 'public/laundry-bg.jpg' ];
 
 self.addEventListener('install', e => {
