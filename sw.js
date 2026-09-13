c3942db03d72bb1cd39834e0f9fc6c22ed49a2d6
welcome-to-batam <cowokperenung@gmail.com>
2026-09-13 16:17:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gavin-laundry-v30';
+const CACHE_NAME = 'gavin-laundry-v31';
 const ASSETS = [ 'index.html', 'manifest.json', 'public/logo.png', 'public/laundry-bg.jpg' ];
 
 self.addEventListener('install', e => {
